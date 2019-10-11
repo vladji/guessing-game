32e02272513c2d2035e3b4db40d0d9bb08471326
vladji <vladjislav@gmail.com>
2019-10-12 00:03:34 +0300

    feat: guessing-game is done:)

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,43 @@
 class GuessingGame {
-    constructor() {}
+	constructor() {
+		this.leftSide = null;
+		this.rightSide = null;
+		this.min = null;
+		this.max = null;
+		this.result = null;
+	}
 
-    setRange(min, max) {
+	setRange(min, max) {
+		this.min = min;
+		this.max = max;
 
-    }
+		const indexMax = max - 1;
+		let indexMin = null;
+		if (min === 0) {
+			indexMin = min;
+		} else {
+			indexMin = min - 1;
+		}
 
-    guess() {
+		const halfRange = Math.floor((indexMax + indexMin) / 2);
+		const num = halfRange + 1;
+		this.result = num;
 
-    }
+		this.leftSide = num;
+		this.rightSide = num + 1;
+	}
 
-    lower() {
+	guess() {
+		return this.result;
+	}
 
-    }
+	lower() {
+		this.setRange(this.min, this.leftSide);
+	}
 
-    greater() {
-
-    }
+	greater() {
+		this.setRange(this.rightSide, this.max);
+	}
 }
 
 module.exports = GuessingGame;
